0f3371ae8614c5a92cbf5dd9d85a8aae41aa30b0
Timothy Do <timothy.doK2004@gmail.com>
2024-08-08 22:31:34 -0700

    added hover color for active and inactive tabs at the bottom

--- screens/Welcome.js
+++ screens/Welcome.js
@@ -2,47 +2,50 @@ import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { AntDesign, FontAwesome, Entypo, MaterialIcons } from '@expo/vector-icons';
 
-//Import Screens
+// Import Screens
 import Cookbook from "./../screens/Cookbook";
 import Planner from "./../screens/Planner";
 import Search from "./../screens/Search";
 import Shop from "./../screens/Shop";
-import Profile from "./../screens/Profile";
 import Login from "./../screens/Login";
-
 
 const Tab = createBottomTabNavigator();
 
-const Welcome = ({navigation}) => {
+const Welcome = ({ navigation }) => {
     return (
-        <Tab.Navigator initialRouteName="Search">
-            <Tab.Screen name="Cookbook" component={Cookbook} options={{
-                tabBarIcon: ({ color, size }) => (
-                    <AntDesign name="book" size={24} color="black" />
-                )
-            }} />
-            <Tab.Screen name="Planner" component={Planner} options={{
-                tabBarIcon: ({ color, size }) => (
-                    <FontAwesome name="calendar" size={24} color="black" />
-                )
-            }} />
-            <Tab.Screen name="Search" component={Search} options={{
-                tabBarIcon: ({ color, size }) => (
-                    <AntDesign name="search1" size={24} color="black" />
-                )
-            }} />
-            <Tab.Screen name="Shop" component={Shop} options={{
-                tabBarIcon: ({ color, size }) => (
-                    <Entypo name="shop" size={24} color="black" />
-                )
-            }} />
-            <Tab.Screen name="Profile" component={Login} options={{
-                tabBarIcon: ({ color, size }) => (
-                    <MaterialIcons name="account-circle" size={24} color="black" />
-                )
-            }} />
+        <Tab.Navigator
+            initialRouteName="Search"
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ color, size }) => {
+                    let iconName;
+                    if (route.name === 'Cookbook') {
+                        iconName = 'book';
+                        return <AntDesign name={iconName} size={size} color={color} />;
+                    } else if (route.name === 'Planner') {
+                        iconName = 'calendar';
+                        return <FontAwesome name={iconName} size={size} color={color} />;
+                    } else if (route.name === 'Search') {
+                        iconName = 'search1';
+                        return <AntDesign name={iconName} size={size} color={color} />;
+                    } else if (route.name === 'Shop') {
+                        iconName = 'shop';
+                        return <Entypo name={iconName} size={size} color={color} />;
+                    } else if (route.name === 'Profile') {
+                        iconName = 'account-circle';
+                        return <MaterialIcons name={iconName} size={size} color={color} />;
+                    }
+                },
+                tabBarActiveTintColor: 'royalblue', // Change this color to your desired active tab icon color
+                tabBarInactiveTintColor: 'powderblue', // Change this color to your desired inactive tab icon color
+            })}
+        >
+            <Tab.Screen name="Cookbook" component={Cookbook} />
+            <Tab.Screen name="Planner" component={Planner} />
+            <Tab.Screen name="Search" component={Search} />
+            <Tab.Screen name="Shop" component={Shop} />
+            <Tab.Screen name="Profile" component={Login} />
         </Tab.Navigator>
-    )
+    );
 };
 
 export default Welcome;
